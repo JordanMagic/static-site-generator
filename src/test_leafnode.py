48187a672a96f95cceb134ea68391aa3a64e7e4a
JordanMagic <jo28magic@gmail.com>
2025-04-24 23:46:17 +0100

    added leadnode class

---
+++ src/test_leafnode.py
@@ -0,0 +1,12 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com">Click me!</a>')
